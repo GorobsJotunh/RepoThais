f363acf3d5505f7dc0365948e07688ac79b306b1
Dawid <gorobsjotunh@gmail.com>
2025-04-19 01:55:24 +0200

    Test

---
+++ Zadanie 1.py
@@ -0,0 +1,8 @@
+# Zadanie 1
+
+number_1 = int(input("Wybierz liczbe:"))
+number_2 = int(input("Wybierz liczbe:"))
+
+summary = number_1 * number_2
+
+print("Wynik to:", summary)
---
+++ Zadanie 2.py
@@ -0,0 +1,9 @@
+# Zadanie 2
+
+name = ""
+print(bool(name))
+
+if bool(name) == True:
+    print("Tak, tak masz na imie")
+else:
+    print("Przedstaw sie kurwo")
